6b1f3b100f738f96feb3af70cb761fa63dc49828
fatihjr5 <74552760+fatihjr5@users.noreply.github.com>
2021-10-21 11:22:54 +0700

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+$('.hero').parallax({ imageSrc: '/assets/img/hero.png' });
+$('.reservation').parallax({ imageSrc: '/assets/img/book.png' });
+$('.menu-1').parallax({ imageSrc: '/assets/img/salad-min.png' });
+$('.menu-2').parallax({ imageSrc: '/assets/img/steak-min.png' });
+$('.menu-3').parallax({ imageSrc: '/assets/img/beverages-min.png' });
